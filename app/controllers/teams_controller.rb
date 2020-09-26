0d6d3f142ac5fe29b2d2dc2e8b82f99093b26957
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-09-26 16:28:48 -0300

    Adicionando controller teams e rotas

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,64 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+  # GET /teams
+  def index
+    @teams = Team.all
+  end
+
+  # GET /teams/1
+  def show
+  end
+
+  # GET /teams/new
+  def new
+    @team = Team.new
+  end
+
+  # GET /teams/1/edit
+  def edit
+  end
+
+  # POST /teams
+  def create
+    @team = Team.new(team_params)
+
+    respond_to do |format|
+      if @team.save
+        format.html { redirect_to @team, notice: 'Team was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /teams/1
+  def update
+    respond_to do |format|
+      if @team.update(team_params)
+        format.html { redirect_to @team, notice: 'Team was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /teams/1
+  def destroy
+    @team.destroy
+    respond_to do |format|
+      format.html { redirect_to teams_url, notice: 'Team was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_team
+      @team = Team.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def team_params
+      params.require(:team).permit(:name, :coach_id)
+    end
+end
